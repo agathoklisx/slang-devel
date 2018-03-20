810a718f0bbfebe8adc4014fa997c6662d0ffd0f
aga.x <aga.chatzimanikas@gmail.com>
2018-03-21 00:44:50 +0200

    Added realpath(3) implementation

---
+++ clib/realpath.c
@@ -0,0 +1,38 @@
+/* realpath(3) implementation
+ *
+ * Originally written by Agathoklis D.E. Chatzimanikas
+ */
+
+#include <limits.h>
+#include <stdlib.h>
+
+static void realpath_intrin (char *path)
+{
+  long path_max;
+  char *p;
+
+#ifdef PATH_MAX
+  path_max = PATH_MAX;
+#else
+  path_max = pathconf (path, _PC_PATH_MAX);
+  if (path_max <= 0)
+    path_max = 4096;
+#endif
+
+  if (NULL == (p = (char *)SLmalloc (path_max+1)))
+    return;
+
+  if (NULL != realpath (path, p))
+    {
+    (void) SLang_push_malloced_string (p);
+    return;
+    }
+
+   SLerrno_set_errno (errno);
+   SLfree (p);
+   (void) SLang_push_null ();
+}
+
+/* signature
+  MAKE_INTRINSIC_S("realpath", realpath_intrin, SLANG_VOID_TYPE),
+ * /
